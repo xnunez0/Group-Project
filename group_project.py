5a383b08f2dad5fb6e9622ac4e3f2fe0e246ef40
Xavier Nunez <xaviernunez83@gmail.com>
2021-05-12 20:03:08 -0700

    finished pseudo-code

--- group_project.py
+++ group_project.py
@@ -9,3 +9,29 @@ It will then display all colors that start with that letter.
 
 """
 
+"""
+colors = list
+
+main()
+letter = request_letter
+fill_list _with_colors
+display_colors
+
+request_letter():
+letter = input("enter the first letter: ")
+return letter.upper()
+
+fill_list _with _colors(letter):
+colors
+infile =open("Colors.txt", 'r')
+for color in infile:
+    if color startswith(letter):
+        colors.append(color.rstrip())
+    infile.close()
+
+def displayColors():
+    for color in colors:
+        print(color)        
+
+main()
+"""
