c4fb8d298555058ec71632282253315d13af1ba2
Xavier Nunez <xaviernunez83@gmail.com>
2021-05-12 19:51:46 -0700

    first commit

---
+++ group_project.py
@@ -0,0 +1,12 @@
+"""
+Were making a program that goes through a list of 
+colors and seperates them based off their first letter.
+Were going to ask the user for a letter
+the program will go through the .txt file and
+sperate the colors in the list that begin with the user input.
+It will then display all colors that start with that letter. 
+
+
+"""
+
+
